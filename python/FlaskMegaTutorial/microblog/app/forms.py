2c25427ee4f16410e0885be6ec2d59990e5c2641
Nick <slxecas@gmail.com>
2013-10-10 00:53:20 -0400

    New flask tutorial stuff

---
+++ python/FlaskMegaTutorial/microblog/app/forms.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.3
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField
+from wtforms.validators import Required
+
+class LoginForm(Form):
+    openid = TextField('openid', validators=[Required()])
+    remember_me = BooleanField('remember_me', default=False)
