0a3a0c2fd20cccf65b3d70e627eac5c5eb4d89db
Nick "Slxe" Donais <slxecas@gmail.com>
2015-03-23 16:23:37 -0400

    Rust more

---
+++ rust/rustbyexample/7_6-if_let.rs
@@ -0,0 +1,33 @@
+fn main() {
+    // All have type `Option<i32>`
+    let number = Some(7);
+    let letter: Option<i32> = None;
+    let emoticon: Option<i32> = None;
+
+    // The `if let` construct reads: "if `let` destructures `number` into `Some(i)`, evaluate the
+    // block (`{}`). Else, do nothing."
+    if let Some(i) = number {
+        println!("Matched {:?}!", i);
+    }
+
+    // If you need to specify a failure, use an else:
+    if let Some(i) = letter {
+        println!("Matched: {:?}!", i);
+    } else {
+        // Destructure failed, change the failure case.
+        println!("Didn't match a number. Let's go with a letter!");
+    }
+
+    // Provide an altered failing condition.
+    let i_like_letters = false;
+
+    if let Some(i) = emoticon {
+        println!("Matched {:?}!", i);
+        // Destructure failed, evaluated the condition to see if this branch should be taken.
+    } else if i_like_letters {
+        println!("Didn't match a number. Let's go with a letter!");
+        // The condition evaluated false, this branch is the default
+    } else {
+        println!("I don't like letters. Let's go with an emoticon :@!");
+    }
+}
